f34f601a48e861c412ef84e6017b34400c5665ed
Dulio Denis <dulio.denis@yahoo.com>
2016-05-08 13:12:29 -0400

    Set-up game variables in the game scene

--- Boxer/Boxer/GameScene.swift
+++ Boxer/Boxer/GameScene.swift
@@ -8,6 +8,35 @@
 
 import SpriteKit
 
+// Game Variables
+
+var box = SKSpriteNode?()
+var floor = SKSpriteNode?()
+var boxSize = CGSize(width: 130, height: 130)
+
+var mainLabel = SKLabelNode?()
+var scoreLabel = SKLabelNode?()
+var boxColor = SKColor?()
+
+var isSpawning = true
+var isCollecting = false
+var canComplete = false
+var logicVar = false
+
+var baseColor: CGFloat = 0.5
+
+var offWhite = SKColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.0)
+var offBlack = SKColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1.0)
+
+var touchLocation = CGPoint?()
+var score = 0
+
+var countdownTimer = 6
+var topCountdownTimer = 5
+
+var touchedNode = SKNode?()
+
+
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
  
